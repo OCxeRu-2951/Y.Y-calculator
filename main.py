61d88d2cc4851d7736e2baae9272b15ee6546613
OCxeRu-2951 <k2000.xxx0@gmail.com>
2024-09-22 17:38:48 +0000

    update

--- main.py
+++ main.py
@@ -223,13 +223,6 @@ def channel(channelid:str,response: Response,request: Request,yuki: Union[str] =
     t = get_channel(channelid)
     return template("channel.html", {"request": request,"results":t[0],"channelname":t[1]["channelname"],"channelicon":t[1]["channelicon"],"channelprofile":t[1]["channelprofile"],"proxy":proxy})
 
-@app.get("/c", response_class=HTMLResponse)
-async def set_cokie(q: str, request: Request):
-    response = RedirectResponse(url="/")
-    response.set_cookie(key="yuki", value="True", max_age=31536000, path="/")
-    
-    return response
-
 @app.get("/playlist", response_class=HTMLResponse)
 def playlist(list:str,response: Response,request: Request,page:Union[int,None]=1,yuki: Union[str] = Cookie(None),proxy: Union[str] = Cookie(None)):
     if not(check_cokie(yuki)):
